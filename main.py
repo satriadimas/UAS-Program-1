59eb4aac2adcb9cee451a1ad74fca03df4c5cb5d
satriadimas <satriadimasp@gmail.com>
2021-01-11 20:51:14 +0700

    ubah main.py

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from model.daftar_nilai import *
 from view.view_nilai import *
 
 while True:
-    c = input("\nA)dd, E)dit, S)earch, D)elete L)ist, Q)uit: ")
+    i = input("\nA)dd, E)dit, S)earch, D)elete L)ist, Q)uit: ")
 
-    if (c.lower() == 'a'):
+    if (i.lower() == 'a'):
         tambah_data()
 
-    elif (c.lower() == 'e'):
+    elif (i.lower() == 'e'):
         ubah_data()
 
-    elif (c.lower() == 's'):
+    elif (i.lower() == 's'):
         cetak_hasil_pencarian()
 
-    elif (c.lower() == 'd'):
+    elif (i.lower() == 'd'):
         hapus_data()
 
-    elif (c.lower() == 'l'):
+    elif (i.lower() == 'l'):
         cetak_daftar_nilai()
 
-    elif (c.lower() == 'q'):
+    elif (i.lower() == 'q'):
         break
 
     else:
